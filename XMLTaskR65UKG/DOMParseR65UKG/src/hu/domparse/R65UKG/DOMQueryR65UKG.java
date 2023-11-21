8e615cf2b305f16225ca239af903e1c9c1ed5c16
Szaszu2002 <szaboalexandram18@gmail.com>
2023-11-21 19:08:07 +0100

    beginning DOM

---
+++ XMLTaskR65UKG/DOMParseR65UKG/src/hu/domparse/R65UKG/DOMQueryR65UKG.java
@@ -0,0 +1,23 @@
+package hu.domparse.R65UKG;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+public class DOMQueryR65UKG {
+
+	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException, TransformerException {
+    }
+}
