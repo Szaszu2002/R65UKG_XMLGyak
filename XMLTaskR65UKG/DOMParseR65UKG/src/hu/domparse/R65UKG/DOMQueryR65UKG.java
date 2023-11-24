be36d8725ab615528f2ab8dd6fb216972fec6f66
Szaszu2002 <szaboalexandram18@gmail.com>
2023-11-24 12:33:45 +0100

    DOMQuery main()

--- XMLTaskR65UKG/DOMParseR65UKG/src/hu/domparse/R65UKG/DOMQueryR65UKG.java
+++ XMLTaskR65UKG/DOMParseR65UKG/src/hu/domparse/R65UKG/DOMQueryR65UKG.java
@@ -6,12 +6,6 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
@@ -19,5 +13,57 @@ import org.xml.sax.SAXException;
 public class DOMQueryR65UKG {
 
 	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException, TransformerException {
+        File xmlFile = new File("XMLR65UKG.xml");
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+
+        Document doc = dbBuilder.parser(xmlFile);
+        doc.getDocumentElement().normalize();
+
+        System.out.println("1. Azok a tárgyak, melyekből elmélet és gyakorlat is van egyaránt: ");
+
+        NodeList targyList= doc.getElementsByTagname("Tárgyak");
+        for(int i=0; i<targyList.getLength(); i++){
+            Node node = targyList.item(i);
+            printTargy(node, "elmélet és gyakorlat");
+        }
+
+        System.out.println("");
+        System.out.println("2. Azok az oktatok, melyeknek az Informatikai épületben van a szobájuk: ");
+
+        NodeList oktatoList = doc.getElementsByTagname("Oktatók");
+        for(int i=0; i<oktatoList.getLength(); i++){
+            Node node = oktatoList.item(i);
+            printOktato(node, "Informatikai épület");
+        }
+
+        System.out.println("");
+        System.out.println("3. Azok a termek, ahol 100-nál nagyobb a férőhely: ");
+
+        NodeList teremList = doc.getElementsByTagname("Terem");
+        for(int i=0; i<teremList.getLength(); i++){
+            Node node = teremList.item(i);
+            printOktato(node, 100);
+        }
+
+        System.out.println("");
+        System.out.println("4. Azok a termek, ahol van vetítő: ");
+
+        for(int i=0; i<teremList.getLength(); i++){
+            Node node = teremList.item(i);
+            printOktato(node, "Van");
+        }
+
+        System.out.println("");
+        System.out.println("5. A Gépészmérnőki és Informatikai Kar szakjai: ");
+
+        NodeList karList = doc.getElementsByTagname("Kar");
+        for(int i=0; i<karList.getLength(); i++){
+            Node node = karList.item(i);
+            printOktato(node, "Gépészmérnőki és Informatikai Kar");
+        }
+
+
     }
 }
