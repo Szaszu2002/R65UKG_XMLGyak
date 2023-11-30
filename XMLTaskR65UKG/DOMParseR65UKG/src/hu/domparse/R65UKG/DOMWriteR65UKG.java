00173acbaea26a873fa2fd91a0e3d59dd3eb13c6
Szaszu2002 <szaboalexandra18@gmail.com>
2023-11-30 08:52:03 +0100

    DOMWrite

--- XMLTaskR65UKG/DOMParseR65UKG/src/hu/domparse/R65UKG/DOMWriteR65UKG.java
+++ XMLTaskR65UKG/DOMParseR65UKG/src/hu/domparse/R65UKG/DOMWriteR65UKG.java
@@ -2,6 +2,7 @@ package hu.domparse.R65UKG;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,7 +18,45 @@ import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 public class DOMWriteR65UKG {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException, TransformerException{
+        File xmFile = new File("XMLTaskR65UKG/DOMParseR65UKG/src/hu/domparse/R65UKG/XMLR65UKG.xml");
+
+        DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dFactory.newDocumentBuilder();
+
+        Document document = dBuilder.parse(xmFile);
+        document.normalize();
+
+        //Lemenjük a dokumentumot
+        saveDocument(document, "write.txt");
+
     }
+
+    public static void saveDocument(Document document, String filename) throws TransformerException {
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transf = transformerFactory.newTransformer();
+		
+		transf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		transf.setOutputProperty(OutputKeys.INDENT, "yes");
+		transf.setOutputProperty("{https://xml.apache.org/.xslt}indent-amount", "2");
+		
+		DOMSource source = new DOMSource(document);
+		
+		File myFile = new File(filename);
+		PrintStream p = null;
+        try{
+            p = new PrintStream(System.out, true,"UTF-8");
+        }
+        catch(Exception e){
+            System.out.println("Don't make it!");
+            return;
+        }
+        
+		StreamResult console = new StreamResult(p);
+        
+		StreamResult file = new StreamResult(myFile);
+		
+		transf.transform(source, console);
+		transf.transform(source, file);
+	}
 }
