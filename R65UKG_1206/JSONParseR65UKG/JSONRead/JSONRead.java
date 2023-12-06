79100dd7c2de4df4f703c7806655ccbf4b123ea7
Szabó Alexandra Marianna <90461871+Szaszu2002@users.noreply.github.com>
2023-12-06 13:29:50 +0100

    1206

---
+++ R65UKG_1206/JSONParseR65UKG/JSONRead/JSONRead.java
@@ -0,0 +1,12 @@
+package R65UKG;
+
+import java.util.Map;
+import json.simple;
+
+public class JSONRead {
+
+	public static void main(String[] args) {
+        JsonParser parser = Json.createParser(new StringReader("[kurzusfelvetel.json]"));
+        JSONObject(String source) ///Hozzon létre egy JSONObject-et egy forrás JSON szöveges karakterláncból.
+    }
+}
---
+++ R65UKG_1206/JSONParseR65UKG/JSONWrite/JSONWrite.java
@@ -0,0 +1,8 @@
+package R65UKG;
+
+public class JSONWrite {
+
+	public static void main(String[] args) {
+        JsonParser parser = Json.createParser(new StringReader("[kurzusfelvetel.json]"));
+    }
+}
