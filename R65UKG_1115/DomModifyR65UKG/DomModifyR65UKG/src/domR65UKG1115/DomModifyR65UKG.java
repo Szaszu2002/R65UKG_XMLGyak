1ae0c0487525d2ea3958710d430726e8951b1487
Szaszu2002 <szaboalexandra18@gmail.com>
2023-12-10 13:53:08 +0100

    1115 Modify

--- R65UKG_1115/DomModifyR65UKG/DomModifyR65UKG/src/domR65UKG1115/DomModifyR65UKG.java
+++ R65UKG_1115/DomModifyR65UKG/DomModifyR65UKG/src/domR65UKG1115/DomModifyR65UKG.java
@@ -13,7 +13,7 @@ import org.w3c.dom.*;
 
 public class DomModifyR65UKG{
     public static void main(String[] args) throws Exception {
-        File xmlFile = new File("R65UKG_1115/DomModifyR65UKG/src/domR65UKG1115/R65UKG_kurzusfelvetel.xml")
+        File xmlFile = new File("R65UKG_1115/DomModifyR65UKG/src/domR65UKG1115/R65UKG_kurzusfelvetel.xml");
         
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
@@ -23,8 +23,16 @@ public class DomModifyR65UKG{
 
         Element root =document.getDocumentElement();
 
+        //a
         Element kurzus = (Element)document.getElementsByTagName("kurzus").item(0);
         createOraado(document);
+
+        //b
+        modifyLanguage(document, "angol");
+
+        //c
+        Element hallgato = (Element)document.getElementsByTagName("hallgato").item(0);
+        root.insertBefore(createhallgato(document, "hallgato", "Görög Krisztina", "2002"),hallgato);
 
 
         saveDocument(document, "kurzusfelvetelModify1R65UKG.xml");
@@ -58,7 +66,36 @@ public class DomModifyR65UKG{
 		transf.transform(source, file);
 	}
 
-    public static void createOraado(Document document){
+    public static Node createOraado(Document document){
         
+    }
+
+    public static void modifyLanguage(Document document, String language){
+        Element element = document.createElement("kurzus");
+
+        for(int i=0;i<list.getLength();i++){
+            Node node = list.item(i);
+            if(node.getNodeType() == Node.ELEMENT_NODE){
+                if(node.getAttributes.getNamedItem("nyelv").getTextContent().equals(language)){
+                    node.getAttributes.getNamedItem("nyelv").getTextContent("német");
+                }
+            }
+        }
+    }
+
+    public static Node createhallgato(Document document, String table, String name, String year){
+        Element element = document.createElement(table);
+
+        element.appendChild(createElement(document, "hnev", name));
+        element.appendChild(createElement(document, "szulev", year));
+
+        return element;
+    }
+
+    private static Node createElement(Document document, String name, String value){
+        Element element = document.createElement(name);
+        element.appendChild(document.createTextNode(value));
+
+        return element;
     }
 }
